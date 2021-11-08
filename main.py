09f6e6aea0c410f6120d8ac4dae9d5c60a5d6a8a
aka-nao <93855959+aka-nao@users.noreply.github.com>
2021-11-08 23:37:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
 from flask import Flask
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+
 
 app = Flask(__name__)
-email = os.environ['email']
-pw = os.environ['pw']
-
-
-def reload():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument(
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36')
-    driver = webdriver.Chrome(options=options)
-
-    driver.get("https://moneyforward.com/sign_in")
-    driver.find_element_by_class_name("_11ZPO93b").click()
-    driver.find_element_by_name("mfid_user[email]").send_keys(email)
-    driver.find_element_by_class_name("submitBtn").click()
-    driver.find_element_by_name("mfid_user[password]").send_keys(pw)
-    driver.find_element_by_class_name("submitBtn").click()
-
-    driver.get("https://moneyforward.com/accounts")
-    reload_btns = driver.find_elements(
-        By.XPATH, '//*[@data-disable-with="更新"]')
-    for btn in reload_btns:
-        btn.click()
-    driver.close()
 
 
 @app.route("/")
 def hello():
-    reload()
     return "Hello, world!!"
 
 
