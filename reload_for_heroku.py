ee3e6057b247ee3ad8ec2c970684d93f53fd1147
aka-nao <93855959+aka-nao@users.noreply.github.com>
2022-08-02 16:55:09 +0900

    Add files via upload

--- reload_for_heroku.py
+++ reload_for_heroku.py
@@ -1,6 +1,7 @@
 # %%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager  # バージョンに合わせて自動アップデート
 from selenium.webdriver.chrome.options import Options
 import os
 
@@ -12,35 +13,20 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument(
     '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36')
+# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
-try:
-    driver = webdriver.Chrome(options=options)
-    try:
-        # ログイン
-        driver.get("https://moneyforward.com/sign_in")
-        try:
-            driver.find_element(By.XPATH, '//*[text()="メールアドレスでログイン"]').click()
-            try:
-                driver.find_element(By.XPATH, '//*[@name="mfid_user[email]"]').send_keys(email)
-                driver.find_element(By.XPATH, '//*[contains(@class,"submitBtn")]').click()
-                driver.find_element(By.XPATH, '//*[@name="mfid_user[password]"]').send_keys(pw)
-                driver.find_element(By.XPATH, '//*[contains(@class,"submitBtn")]').click()
-                try:
-                    # 更新
-                    driver.get("https://moneyforward.com/accounts")
-                    reload_btns = driver.find_elements(By.XPATH, '//*[@data-disable-with="更新"]')
-                    for btn in reload_btns:
-                        btn.click()
-                except:
-                    print(f"Error : all reload")
-            except:
-                print(f"Error : email&password send_keys, submit")
-        except:
-            print(f"Error : メールアドレスでログイン")
-    except:
-        print(f"Error : access https://moneyforward.com/sign_in")
-except:
-    print(f"Error : selenium start driver")
-
+driver.get("https://moneyforward.com/sign_in")
+driver.find_element_by_class_name("_11ZPO93b").click()
+driver.find_element_by_name("mfid_user[email]").send_keys(email)
+driver.find_element_by_class_name("submitBtn").click()
+driver.find_element_by_name("mfid_user[password]").send_keys(pw)
+driver.find_element_by_class_name("submitBtn").click()
+# %%
+# 更新
+driver.get("https://moneyforward.com/accounts")
+reload_btns = driver.find_elements(By.XPATH, '//*[@data-disable-with="更新"]')
+for btn in reload_btns:
+    btn.click()
 driver.close()
